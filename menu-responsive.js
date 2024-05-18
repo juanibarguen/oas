b52268712a063b24b2d60f011265d1ac1e005397
juanibarguen <juanibarguen159@gmail.com>
2024-05-18 18:00:50 +0200

    se agrego el favicon y se cambiaron los nombres de las diferentes paginas

--- menu-responsive.js
+++ menu-responsive.js
@@ -1,44 +1,43 @@
+let cambio = false;
+
 let lastScrollTop = 0;
-console.log("asd");
 const navbar = document.getElementById('nav');
 const navbarHeight = navbar.offsetHeight;
 
 window.addEventListener('scroll', function() {
+  if (!cambio) { // Verificar si cambio es false
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
-    // Scroll hacia abajo
-    navbar.style.top = `-${navbarHeight}px`;
-  } else {
-    // Scroll hacia arriba
-    navbar.style.top = '0';
-  }
+    if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
+      // Scroll hacia abajo
+      navbar.style.top = `-${navbarHeight}px`;
+    } else {
+      // Scroll hacia arriba
+      navbar.style.top = `0`;
+    }
   
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
 });
 
 
 
 document.getElementById('menu-responsive').addEventListener('click', function() {
-    console.log('ABRIR');  // Imprimir "hola" en la consola
+  cambio=true;
+  console.log(cambio);
+    //console.log('ABRIR');  // Imprimir "hola" en la consola
 
     var navLinks = document.getElementById('navLinks');
-    var asd = document.getElementById('nav');
-
-    console.log("aaaa");
+    //console.log("aaaa");
     navLinks.classList.add('expand');
 });
 
 
 document.getElementById('menu-responsive-closed').addEventListener('click', function() {
-    console.log('CERRAR');  // Imprimir "hola" en la consola
-
-
+    //console.log('CERRAR');  // Imprimir "hola" en la consola
+  cambio=false;
+  console.log(cambio);
     var navLinks = document.getElementById('navLinks');
-    var asd = document.getElementById('nav');
-
-    navLinks.classList.remove('expand');
     navLinks.classList.remove('expand');
     
 });
